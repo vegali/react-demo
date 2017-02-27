fa5b7e380e9f5da3c6cde833933b5ed42b07e7ca
vegali <li.jianyong@jyall.com>
2017-02-27 14:51:54 +0800

    styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+let MyComponentStyles = {
+    color : 'blue',
+    fontSize : '50px'
+};
 
 ReactDom.render(
-    <h1>Hello World</h1>,document.getElementById('app')
+    <h1 style = {MyComponentStyles}>Hello World</h1>,document.getElementById('app')
 );
