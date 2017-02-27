919606bff435b0409a6b7a30e3f94712832b2f8c
vegali <li.jianyong@jyall.com>
2017-02-27 14:42:14 +0800

    hello world

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <h1>Hello World</h1>,document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var config = {
+    entry : {
+        index : [
+            path.resolve(__dirname, 'src/index.js')
+        ]
+    },
+    output : {
+        path : path.resolve(__dirname,'build'),
+        filename : "[name].bundle.js?[hash]"
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.js$/,loader : ['react-hot-loader','babel-loader'],exclude : path.resolve(__dirname, 'node_modules')
+            }
+        ]
+    },
+    plugins : [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title : 'react-demos',
+            template : './src/index.html'
+        })
+    ]
+};
+
+module.exports = config;
