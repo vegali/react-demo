63fb56d73de9b31abf551dd75f4be2fda79e505f
vegali <li.jianyong@jyall.com>
2017-03-02 22:00:12 +0800

    refs bug

--- src/component/ref-demo.js
+++ src/component/ref-demo.js
@@ -5,25 +5,25 @@ class Refs extends Component{
     state = {
         red : 0,
         green : 0,
-        pink : 0
+        blue : 0
     };
 
     update = (e)=>{
         this.setState({
             red : findDOMNode(this.refs.red).value,
             green : findDOMNode(this.refs.green).value,
-            pink : findDOMNode(this.refs.pink).value
+            blue : findDOMNode(this.refs.blue).value
         });
-        findDOMNode(this.refs.color).style.backgroundColor = `rgb(${this.state.red},${this.state.green},${this.state.pink})`;
+        findDOMNode(this.refs.color).style.backgroundColor = `rgb(${this.state.red},${this.state.green},${this.state.blue})`;
     };
 
     render(){
         let Styles = {width:'100px',height:'100px',backgroundColor:'#eee'};
         return (
             <div>
-                <Slider ref="red" update={this.update}>{this.state.red}</Slider>
-                <Slider ref="green" update={this.update}>{this.state.green}</Slider>
-                <Slider ref="pink" update={this.update}>{this.state.pink}</Slider>
+                <Slider ref="red" update={this.update} value={this.state.red} />{this.state.red}<br />
+                <Slider ref="green" update={this.update} value={this.state.green} />{this.state.green}<br />
+                <Slider ref="blue" update={this.update} value={this.state.blue} />{this.state.blue}<br />
                 <div ref="color" style={Styles}></div>
             </div>
         )
@@ -34,6 +34,7 @@ class Slider extends Component{
     render(){
         return(
             <input type="range"
+                defaultValue = {this.props.value}
                 min="0"
                 max="255"
                 onChange={this.props.update}
